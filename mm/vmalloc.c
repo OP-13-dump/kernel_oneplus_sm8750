6bc51e058cc06e3086285396236a765e7e121a0c
whitewhale0612 <2136512929@qq.com>
2026-01-20 02:38:45 +0800

    mm/vmalloc: move resched point into alloc_vmap_area() Currently vm_area_alloc_pages() contains two cond_resched() points. However, the page allocator already has its own in slow path so an extra resched is not optimal because it delays the loops.
    The place where CPU time can be consumed is in the VA-space search in
    alloc_vmap_area(), especially if the space is really fragmented using
    synthetic stress tests, after a fast path falls back to a slow one.
    
    Move a single cond_resched() there, after dropping free_vmap_area_lock in
    a slow path.  This keeps fairness where it matters while removing
    redundant yields from the page-allocation path.
    
    Signed-off-by: NoCache-69 <chethangowdasj86@gmail.com>

--- mm/vmalloc.c
+++ mm/vmalloc.c
@@ -1634,6 +1634,7 @@ retry:
 	addr = __alloc_vmap_area(&free_vmap_area_root, &free_vmap_area_list,
 		size, align, vstart, vend);
 	spin_unlock(&free_vmap_area_lock);
+	cond_resched();
 
 	trace_alloc_vmap_area(addr, size, align, vstart, vend, addr == vend);
 
@@ -3081,7 +3082,6 @@ vm_area_alloc_pages(gfp_t gfp, int nid,
 							pages + nr_allocated);
 
 			nr_allocated += nr;
-			cond_resched();
 
 			/*
 			 * If zero or pages were obtained partly,
@@ -3129,7 +3129,6 @@ vm_area_alloc_pages(gfp_t gfp, int nid,
 		for (i = 0; i < (1U << order); i++)
 			pages[nr_allocated + i] = page + i;
 
-		cond_resched();
 		nr_allocated += 1U << order;
 	}
 
